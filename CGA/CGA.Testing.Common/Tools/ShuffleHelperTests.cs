d0f573425e21a304527d1c520f793b90d9239283
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-09-17 09:56:05 +0200

    Clean code

--- CGA/CGA.Testing.Common/Tools/ShuffleHelperTests.cs
+++ CGA/CGA.Testing.Common/Tools/ShuffleHelperTests.cs
@@ -1,10 +1,5 @@
 ﻿using CGA.Common.Tools;
 using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CGA.Testing.Common.Tools
 {
