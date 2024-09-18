de2145a2a00388b90b3a1581ace5a68dcb7c4104
Alexey Bubley <alexey.bubley@discovercarhire.com>
2024-09-18 13:05:31 +0200

    Clean code

--- CGA/CGA.Common/Models/Table.cs
+++ CGA/CGA.Common/Models/Table.cs
@@ -9,11 +9,20 @@ namespace CGA.Common.Models
 {
     public class Table
     {
+        public Guid TableId { get; set; }
         public GameTypeEnum GameType { get; set; }
-        public decimal MinimumBet { get; set; }
-        public decimal MaximumBet { get; set; }
-        public int MaximumHands { get; set; }
-        public required List<Player> Players { get; set; }
-        public required Game Game { get; set; }
+        public decimal MinimumBet { get; protected set; }
+        public decimal MaximumBet { get; protected set; }
+        public int MaximumHands { get; protected set; }
+        public List<Player>? Players { get; protected set; }
+        public Game? Game { get; protected set; }
+        public IShuffleMachine? ShuffleMachine { get; protected set; }
+
+        public Table(decimal minBet, decimal maxBet, int maxHands)
+        {
+            MinimumBet = minBet;
+            MaximumBet = maxBet;
+            MaximumHands = maxHands;
+        }
     }
 }
